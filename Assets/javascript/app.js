e446055a1b398593ac2a2425b1fdcfd0ce1ad74e
Dameon1 <dameonmedoza@gmail.com>
2017-11-28 00:16:31 -0700

    empty wells
    simple attachments to html,.

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -13,7 +13,7 @@
     console.log("still working");
       },1000*3);
 
-
+// clearTimeout();
 
 
 
@@ -27,33 +27,51 @@
 
 // create some variables to work with
   let $a = $("#first");
-  let $b = $("#second")
+  let $b = $("#second");
+  let $setTimeout = setTimeout(function(){},1000*5);
 
+  let q1 = {
+    q1: "Who wrote?",
+    q1choices: ["a","b","c","d"],
+};
+  let correct = 0;
+  let wrong   = 0;
+
+ //add question objects to draw from
 
 
 // create classes
 
-class example {
-  constructor(name,attr) {
-    this._name=name;
-    this._attr=attr;
+
+  constructor = (x,y) => {
+    for ( let i = 0; i < y.length; i++) {
+      let question = $("<h3>");
+      question.text(y[i]);
+
+
+      $("#choices").append(question);
+    }
+    $("#question").text(x+"");
   }
-}
 
-
-
- //add question objects to draw from
-
-    let q1 = {
-      q1: "Who wrote?",
-      q1choices: ["a","b","c","d"],
- };
+constructor(q1.q1,q1.q1choices);
 
 
 
 
 
 
+// Ask question q1.q1 with console.log of nextQuestion
+console.log(q1.q1);
+
+//Create a for-loop to iterate through the q1.q1choices array
+
+
+//onClick functionality
+$
+//determine resultsDisplay
+
+//reset timer and ask nextQuestion
 
 // make functions to use, nextQuestion,timerReset,resultsDisplay
 timerReset = () => {
@@ -63,9 +81,36 @@ timerReset = () => {
 
 
 // attach to html
+
+
+
+
+
+
 // display results
 
 
 
 
 });
+/*  for (let i = 0; i < letters.length;i++) {
+      let newDiv = $("<h3>");
+      letterBtn.addClass("letter-button letter-button-color letter");
+      letterBtn.attr("data-letter",letters[i]);
+      letterBtn.text(letters[i]);
+      $("#buttons").append(letterBtn);
+
+
+      }
+
+
+    $(".letter-button").on('click', function(event){
+      let fridgeMagnet = $("<div>");
+      fridgeMagnet.addClass("letter fridge-color").text($(this).attr("data-letter"));
+      $("#display").append(fridgeMagnet);
+})
+
+
+
+$("#clear").on("click",function(){
+  $("#display").empty();*/
