215f913a7c325f89672fe41b6dbaea6f09958297
Dameon1 <dameonmedoza@gmail.com>
2017-11-27 22:30:28 -0700

    init
    init

---
+++ Assets/javascript/app.js
@@ -0,0 +1,61 @@
+
+
+// document ready... allows the page to load completely before linking anything to the html
+/////////////////....very important
+
+  $(document).ready(function() {
+
+    console.log("working");
+// start with a timer. this is a timed question game
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// create some variables to work with
+  var $a = $("#first");
+
+
+
+
+// create classes
+
+
+
+
+
+ //add question objects to draw from
+
+ var sample = {
+   name:"a",
+   q1: "question",
+   q1choices: ["","","",""],
+ };
+
+
+
+
+
+
+
+// make functions to use, nextQuestion,timerReset,resultsDisplay
+// attach to html
+// display results
+
+
+
+
+});
