60068e5ca49267de020a176bd15a684a1555d4ca
Dameon1 <dameonmedoza@gmail.com>
2017-11-27 23:18:09 -0700

    some direction
    partial layouts

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,17 +1,17 @@
 
 
-// document ready... allows the page to load completely before linking anything to the html
-/////////////////....very important
+/* document ready... allows the page to load completely before linking anything to the html
+              ....very important*/
 
   $(document).ready(function() {
 
+// check to see if working
     console.log("working");
+
 // start with a timer. this is a timed question game
-
-
-
-
-
+  setTimeout(function() {
+    console.log("still working");
+      },1000*3);
 
 
 
@@ -26,23 +26,27 @@
 
 
 // create some variables to work with
-  var $a = $("#first");
-
+  let $a = $("#first");
+  let $b = $("#second")
 
 
 
 // create classes
 
-
+class example {
+  constructor(name,attr) {
+    this._name=name;
+    this._attr=attr;
+  }
+}
 
 
 
  //add question objects to draw from
 
- var sample = {
-   name:"a",
-   q1: "question",
-   q1choices: ["","","",""],
+    let q1 = {
+      q1: "Who wrote?",
+      q1choices: ["a","b","c","d"],
  };
 
 
@@ -52,6 +56,12 @@
 
 
 // make functions to use, nextQuestion,timerReset,resultsDisplay
+timerReset = () => {
+
+}
+
+
+
 // attach to html
 // display results
 
