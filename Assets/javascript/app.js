b5d647074bfebf1863a5a28995087bc9072dc3a4
Dameon1 <dameonmedoza@gmail.com>
2017-11-30 15:41:45 -0700

    ~
    ~

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -19,26 +19,60 @@
 
         let correct = 0;
         let wrong   = 0;
-
+//
 //--- question objects for oop games
   let q1 = {
-    question: "Who wrote?",
-    answers: ["a","b","c","d"],
+    question: "Party in the U.S.A",
+    answers: ["Katy Perry","Miley Cyrus","Jessie Jay","Beyonce"],
 };
 
   let q2 = {
-  question: "Who sang?",
-  answers: ["e","f","g","h"],
+  question: "Baby",
+  answers: ["Justin Beiber","Justin Beiber & The Dream","Christina Milan & The Dream","Christina Milan & Justin Beiber"],
 };
 
   let q3 = {
-  question: "Who produced?",
-  answers: ["i","j","k","l"],
+  question: "What do you want from me",
+  answers: ["Pink","Adam Lambert","Adam Levine","Ed Sheeran"],
+};
+
+let q4 = {
+  question: "Just a little bit of you Heart",
+  answers: ["Harry Styles","Justin Beiber","One Direction","Ed Sheeran"],
+};
+
+let q5 = {
+question: "Right Round",
+answers: ["Flo Rida","Bruno Mars","Ne-Yo","The Dream"],
+};
+
+let q6 = {
+question: "Diamonds",
+answers: ["Rhianna","Kelly Clarkson","Taylor Swift","Sia"],
+};
+let q7 = {
+  question: "Miss Independent",
+  answers: ["Sia","Rhianna","Christina Aguilera","Kelly Clarkson",],
+};
+
+let q8 = {
+question: "Irreplaceable",
+answers: ["Flo -Rida","Ne-Yo","Bruno Mars","Ed Sheeran"],
+};
+
+let q9 = {
+question: "Little Things",
+answers: ["Bruno Mars","One Direction","Ne-Yo","Ed Sheeran"],
+};
+
+let q10 = {
+  question: "Nothing Compares 2 U",
+  answers: ["Prince","Phil Collins","Sinead O'connor","Sting"],
 };
 
 let questionIndex = 0;
-let questions = [q1, q2, q3];
-let answerKey = [1,2,3];
+let questions = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
+let answerKey = [2,2,0,0,1,3,2,1,3,0];
 let userPick;
 
 
@@ -48,179 +82,144 @@ let userPick;
 //---Main Question function
 
   getQuestionAnswers = () => {
-   //timer();
-  $("#choices").empty();
-    for ( let i = 0; i < 4; i++) {
+                    //timer();
+      $("#choices").empty();
+
+      for ( let i = 0; i < 4; i++) {
+
         let choice = $("<h3>");
+
         choice.text(questions[questionIndex].answers[i]);
         choice.attr("value",i);
         choice.addClass('click');
+
         $("#choices").append(choice);
+
       };
-        $(".click").on('click',function() {
+
+      $(".click").on('click',function() {
             //Adding a click for response functionality and logging where index is for scoring purposes
-            userPick = $(this).attr("value");
-                    console.log("Your Pick: " + userPick);
-            userPick = parseInt(userPick);
+          userPick = $(this).attr("value");
 
-              console.log(userPick);
-              results(userPick);
-        });
-    $("#question").text(questions[questionIndex].question+"");
+          //console.log(userPick);
+              //console.log(parseInt(userPick));
+          let    parsed = (parseInt(userPick));
+            if (parsed === answerKey[questionIndex]) {
+                correct++;
+                results();
+                checkIndex();
+              } else {
+                wrong++;
+                results();
+                checkIndex();
+              };
+
+    });
+
+    $("#question").text("Who wrote the song:___"+questions[questionIndex].question+"?");
+
     questionIndex++;
-}
-
-//This is to determine if endGame
-  checkIndex = () => {
-  if (questionIndex ===  3) {
-      questionIndex -= 3;
-      console.log("hi");
-      var playAgain = confirm("Play again?");
-      if (playAgain) {
-        return;
-      } else {
-///this needs to be fixed
-      }
-}
-}
-
-
- /*let resetGame = () => {
-  let correct = 0;
-  let wrong = 0;
-  questionIndex -= 3;
-  timer();
-}
-/*
-something=;
- results = () => {
-   //on clicks value compared to answerKey
-
-   something = this.value;
-   something = parseInt(something);
-
-
-   if (something === answerKey[questionIndex]) {
-   console.log("You Win!!")
- } esle {
- console.log("you lose")
-}
-*/
-currentScore = () => {
-  console.log(correct);
-  console.log(wrong);
-}
-
-//
- results = (arr) => {
+};
 
 
 
+  //This is to determine if endGame
+    checkIndex = () => {
+    if (questionIndex === 10 ) {
+        questionIndex -= 10;
+      let playAgain = confirm("You got: "+correct+ "out of 10.")//reset();
+        if (!playAgain) {
+          return;
+        }//reset();
+  }
 
-   //on clicks value compared to answerKey
-console.log(answerKey.questionIndex);
-   console.log("hi")
-   if (userPick === answerKey[questionIndex]) {
-   console.log("You Win!!")
- } else {
- console.log("you lose")
-}
 }
 
 
+  let reset = () => {
+    let correct = 0;
+    let wrong = 0;
+    getQuestionAnswers();
+
+    //timer();
+  }
 
 
 
 
 
+          //results(userPick);
+          results = () => {
+              console.log("Right Answer:" + questions[questionIndex].answers[answerkey.questionIndex]);
+            //userPick = $(this).attr("value");
+            //console.log("Your Pick: " + userPick);
+
+            console.log("Your Pick: " + userPick);
 
 
 
-/*
-    logging
-    1.document
-    2.main object
-    3.first oject in main object
-    4.first key of first object
-*/
-
-  cl = () => {
-// logs the document
-    console.log(this);
-// logs the main object
-    console.log(questions);
-// logs the first object in the main object with a questionIndex variable
-    console.log(questions[questionIndex]);
-// logs the first key of the first object with a questionIndex variable
-    console.log(questions[questionIndex].question);
-// logs direct call of first key
-    console.log(q1.question);
-// logs direct call to answers array
-    console.log(q1.answers);
-// logs current correct score
-    console.log(correct);
-// logs current wrong score
-    console.log(wrong);
 }
 
-// calling initial stack logs --- enable/disable for bugging
-cl();
+         /*
+             logging
+             1.document
+             2.main object
+             3.first oject in main object
+             4.first key of first object
+         */
 
 
-//function for a timer
-function timer() {
-
-      let timeLeft = 3;
-      let timerId = setInterval(countdown, 1000);
-
-      function countdown() {
-
-        if (timeLeft < 0) {
-          clearInterval(timerId);
-            alert("Out of Time");
-              wrong++;
-              checkIndex();
-              getQuestionAnswers();
-
-
-        } else {
-          $("#timer").text(timeLeft);
-          timeLeft--;
-        }
-      }
-    }
-//created a couple of onclicks for working functinality
-$("#timer").on('click',function() {
-  getQuestionAnswers();
-
-});
-
-$("#test").on('click', function() {
-  //getQuestionAnswers();
-  //resetGame();
-  //resetGame();
-  console.log("working");
-});
-
-$("#choices").on('click', function() {
-
-/*
-userPick = this.value;
-userPick = parseInt(userPick);
+         cl = () => {
+       // logs the document
+           console.log(this);
+       // logs the main object
+           console.log(questions);
+       // logs the first object in the main object with a questionIndex variable
+           console.log(questions[questionIndex]);
+       // logs the first key of the first object with a questionIndex variable
+           console.log(questions[questionIndex].question);
+       // logs direct call of first key
+           console.log(q1.question);
+       // logs direct call to answers array
+           console.log(q1.answers);
+       // logs current correct score
+           console.log(correct);
+       // logs current wrong score
+           console.log(wrong);
+}
 
 
-results();
-*/
+       //function for a timer
+       function timer() {
+
+             let timeLeft = 10;
+             let timerId = setInterval(countdown, 1000);
+
+             function countdown() {
+
+              if (timeLeft < 0) {
+                clearInterval(timerId);
+                  alert("Out of Time");
+                    wrong++;
+                    checkIndex();
+                    getQuestionAnswers();
+                    timer();
+               } else {
+                 $("#timer").text(timeLeft);
+                 timeLeft--;
+               }
+             }
+}
+
+           //This starts the game
+           $("#timer").on('click',function() {
+             getQuestionAnswers();
+             timer();
+           });
+
+        
 
 
-
-
-
-    //display resultsDisplay
-    // reset game option
-    // if yes reset else return
-  getQuestionAnswers();
-});
 //Create a for-loop to iterate through the q1.q1choices array
 //onClick functionality
 //determine resultsDisplay
